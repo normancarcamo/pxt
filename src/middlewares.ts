4d4e7f28a26aebc758c6a21c66b8466ff14d200b
normancarcamo <normancarcamo@gmail.com>
2020-02-17 02:49:49 -0600

    Added reason to the payload sent to the client

--- src/middlewares.ts
+++ src/middlewares.ts
@@ -1,4 +1,4 @@
-import { IRequest, IResponse, INext } from './types';
+import { IRequest, IResponse, INext, IHttpError } from './types';
 
 export function notFound (req: IRequest, res: IResponse, next: INext) {
   throw new Error('Not found.');
@@ -8,6 +8,9 @@ export function notAllowed (req: IRequest, res: IResponse, next: INext) {
   throw new Error('Method Not allowed.');
 }
 
-export function error(err: Error, req: IRequest, res: IResponse, next: INext) {
-  res.json({ error: err.message });
+export function error(err: IHttpError, req: IRequest, res: IResponse, next: INext) {
+  res.status(err.status || 400).json({
+    error: err.message,
+    reason: err.reason
+  });
 }
