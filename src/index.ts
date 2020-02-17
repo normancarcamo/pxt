cca9b6b13b334cc64590503ec56e411364093973
normancarcamo <normancarcamo@gmail.com>
2020-02-17 00:54:17 -0600

    Updated port

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
 import 'module-alias/register';
 import app from './app';
 
-app.listen(3000, () => console.log('Server running on port 3000.'));
+const PORT = process.env.SERVER_PORT || 3000;
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}.`));
