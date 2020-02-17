10293b1eff7aea50e249c724b64ce8f10438ed74
normancarcamo <normancarcamo@gmail.com>
2020-02-17 00:52:59 -0600

    updated testMatch option

--- jest.config.integration.js
+++ jest.config.integration.js
@@ -14,7 +14,7 @@ module.exports = {
   moduleFileExtensions: [ "ts", "tsx", "js", "jsx", "json", "node" ],
   watchPathIgnorePatterns: ["node_modules"],
   testMatch: [
-    "<rootDir>/test/**/(*-|*.)(steps|spec|test).(js|jsx|ts|tsx)?(x)"
+    "**/*.steps.ts"
   ],
   modulePaths: [
     "<rootDir>",
